9854e10dee593f67663bfa326a599a3b499c73fb
phillip jarrar <pjjarrar@gmail.com>
2024-09-13 16:30:20 -0500

    implement navbar using bootstrap

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files (HTML, CSS, JavaScript)
+app.use(express.static(__dirname + '/public'));   
+
+// Handle requests for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);   
+
+});
