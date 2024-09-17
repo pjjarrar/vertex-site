4304307dfb90934f3d2b5d7e3a75792530ceaf68
phillip jarrar <pjjarrar@gmail.com>
2024-09-17 12:24:53 -0500

    add contact form and footer section

---
+++ public/js/dropdown.js
@@ -0,0 +1,7 @@
+const servicesDropdown = document.querySelector('.dropdown-toggle');
+
+servicesDropdown.addEventListener('click', (event) => {
+    // event.preventDefault();
+    // servicesDropdown.click(); // Open the dropdown menu
+    window.location.href = '#services'; // Navigate to the services section
+});
