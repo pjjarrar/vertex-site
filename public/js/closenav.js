22cea10c8a7fa53c5959d9306e916c869dd534b0
phillip jarrar <pjjarrar@gmail.com>
2024-09-25 15:45:43 -0500

    implement delay on navbar closing in mobile view when a nav-link is clicked

--- public/js/closenav.js
+++ public/js/closenav.js
@@ -1,9 +1,28 @@
+// document.querySelectorAll('#top-navbar .nav-link').forEach(link => {
+//     link.addEventListener('click', function() {
+//         // Get the offcanvas element
+//         var offcanvasElement = document.getElementById('top-navbar');
+//         // Hide the offcanvas after click
+//         var offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement); 
+//         offcanvas.hide();
+//     });
+// });
+
 document.querySelectorAll('#top-navbar .nav-link').forEach(link => {
     link.addEventListener('click', function() {
-        // Get the offcanvas element
-        var offcanvasElement = document.getElementById('top-navbar');
-        // Hide the offcanvas after click
-        var offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement); 
-        offcanvas.hide();
+        // Smooth scroll to the target section
+        const target = this.getAttribute('href');
+        if (target && target.startsWith('#')) {
+            const section = document.querySelector(target);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+        // Delay closing the offcanvas menu to ensure scrolling happens
+        setTimeout(() => {
+            var offcanvasElement = document.getElementById('top-navbar');
+            var offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
+            offcanvas.hide();
+        }, 300); // Adjust delay timing if necessary
     });
 });
