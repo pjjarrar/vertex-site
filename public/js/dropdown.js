b0acee59c55a2799d47ee7c8004162ff07ca4ec5
phillip jarrar <pjjarrar@gmail.com>
2024-10-01 10:24:15 -0500

    remove large image files/clutter

--- public/js/dropdown.js
+++
@@ -1,8 +0,0 @@
-const servicesDropdown = document.querySelector('.dropdown-toggle');
-
-servicesDropdown.addEventListener('click', (event) => {
-    // event.preventDefault();
-    // servicesDropdown.click(); // Open the dropdown menu
-    window.location.href = '#services'; // Navigate to the services section
-});
-
