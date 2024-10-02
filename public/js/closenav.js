cee5cfe257929c777432d18a9336daf546825faf
phillip jarrar <pjjarrar@gmail.com>
2024-10-02 15:43:03 -0500

    fixed horizontal scroller on mobile devices

--- public/js/closenav.js
+++ public/js/closenav.js
@@ -1,39 +1,24 @@
-// document.querySelectorAll('#top-navbar .nav-link').forEach(link => {
-//     link.addEventListener('click', function() {
+// changes icon on navbar depending on the state of aria-expanded - used for collapse navbar
 
-//         const targetSection = this.getAttribute('href');
-
-//         // Get the offcanvas element
-//         var offcanvasElement = document.getElementById('top-navbar');
-//         // Hide the offcanvas after click
-
-//         if(targetSection === "#about") {
-//             console.log(offcanvasElement);
+// document.addEventListener("DOMContentLoaded", function() {
+//     const navbarToggler = document.querySelector('.navbar-toggler');
+//     const navbarIcon = document.getElementById('navbar-icon');
+    
+//     navbarToggler.addEventListener('click', function() {
+//         const isCollapsed = navbarToggler.getAttribute('aria-expanded') === 'false';
+        
+//         if (isCollapsed) {
+//             navbarIcon.classList.remove('bi-x');
+//             navbarIcon.classList.add('bi-list');  // Change back to original icon when closed
 //         } else {
-//             var offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement); 
-//             offcanvas.hide();
+//             navbarIcon.classList.remove('bi-grid');
+//             navbarIcon.classList.add('bi-x');  // Change this to whatever icon you want when open
 //         }
 //     });
 // });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarIcon = document.getElementById('navbar-icon');
-    
-    navbarToggler.addEventListener('click', function() {
-        const isCollapsed = navbarToggler.getAttribute('aria-expanded') === 'false';
-        
-        if (isCollapsed) {
-            navbarIcon.classList.remove('bi-x');
-            navbarIcon.classList.add('bi-list');  // Change back to original icon when closed
-        } else {
-            navbarIcon.classList.remove('bi-grid');
-            navbarIcon.classList.add('bi-x');  // Change this to whatever icon you want when open
-        }
-    });
-});
-
-document.querySelectorAll('#top-navbar .nav-link').forEach(link => {
+// closes the navbar when a link is clicked
+document.querySelectorAll('#offcanvasNavbar .nav-link').forEach(link => {
     link.addEventListener('click', function() {
         // Smooth scroll to the target section
         const target = this.getAttribute('href');
@@ -45,10 +30,11 @@ document.querySelectorAll('#top-navbar .nav-link').forEach(link => {
         }
         // Delay closing the offcanvas menu to ensure scrolling happens
         setTimeout(() => {
-            var offcanvasElement = document.getElementById('top-navbar');
+            var offcanvasElement = document.getElementById('offcanvasNavbar');
+            // var offcanvas = bootstrap.Collapse.getInstance(offcanvasElement);
             var offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
             offcanvas.hide();
-        }, 300); // Adjust delay timing if necessary
+        }, 100); // Adjust delay timing if necessary
     });
 });
 
