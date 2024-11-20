6f35c0243c79727cf9c4c87afebe56de2cee56d2
phillip jarrar <pjjarrar@gmail.com>
2024-11-20 16:17:08 -0600

    add variable port for testing env.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files (HTML, CSS, JavaScript)
 app.use(express.static(__dirname + '/public'));   
